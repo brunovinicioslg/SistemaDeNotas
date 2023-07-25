bb9c76da19cb439366b4bc9e8a8416a585f6f86f
brunovinicioslg <brunvinicios775@gmail.com>
2023-07-25 19:45:56 -0300

    # Versão 5.4

---
+++ SistemaDeNotas/Controllers/UsuarioGeralController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaDeNotas.Filters;
+using SistemaDeNotas.Models;
+using SistemaDeNotas.Repositorio;
+
+namespace SistemaDeNotas.Controllers
+{
+    public class UsuarioGeralController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        private readonly INotaRepositorio _notaRepositorio;
+        public UsuarioGeralController(IUsuarioRepositorio usuarioRepositorio,
+                                 INotaRepositorio notaRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+            _notaRepositorio = notaRepositorio;
+        }
+        public IActionResult Index()
+        {
+            List<UsuarioModel> usuarios = _usuarioRepositorio.BuscarTodos();
+
+            return View(usuarios);
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(UsuarioModel usuarios)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    usuarios = _usuarioRepositorio.Adicionar(usuarios);
+                    TempData["MensagemSucesso"] = "Inserido com sucesso";
+                    return RedirectToAction("Index", "Login");
+                }
+                return View(usuarios);
+            }
+            catch (Exception erro)
+            {
+                TempData["MensagemErro"] = $"Falha ao inserir, tente novamente, detalhes:{erro.Message} ";
+                return RedirectToAction("Criar", "UsuarioGeral");
+            }
+
+        }
+    }
+}
