a686b97ea2eb360c4a5c2a602838023c00dedb66
brunovinicioslg <brunvinicios775@gmail.com>
2023-09-02 12:57:32 -0300

    #Versão 10.0.4

--- SistemaDeNotas/wwwroot/js/site.js
+++ SistemaDeNotas/wwwroot/js/site.js
@@ -4,6 +4,7 @@
 // Write your JavaScript code.
 $(document).ready(function () {
 
+
     $('#turmavalidate').on('change', function () {
         validarSelect();
     });
@@ -210,3 +211,21 @@ function validarSelect() {
         removeError(5);
     }
 }
+
+//menu mobile
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
